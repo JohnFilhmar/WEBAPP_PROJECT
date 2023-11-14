948a9320b1a091f5c753c725f73503b50bd0402f
ampelme <olajohnfilhmar@gmail.com>
2023-11-14 19:55:07 +0800

    progress styling

--- app/views/login.php
+++ app/views/login.php
@@ -1,33 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login - JMS</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?=base_url(); ?>public/css/custom.css">
 </head>
+
 <body class="bg-dark">
     <div class="container text-center">
         <div class="card mt-5 bg-secondary">
             <div class="card-header">
-                <h1>LOGIN</h1>
+                <h1>Login to Your Account</h1>
             </div>
             <div class="card-body">
                 <form action="/auth" method="post">
-                    <div class="form-control mt-5 bg-secondary">
-                        <label for="username form-label">Enter userName:</label>
-                        <input required class="w-100" type="text" name="username" id="username">
+                    <div class="form-group mt-4">
+                        <label for="username">Username:</label>
+                        <input required class="form-control" type="text" name="username" id="username">
                     </div>
-                    <div class="form-control mt-5 bg-secondary">
-                        <label for="password">Enter password:</label>
-                        <input required minlength="8" class="w-100" type="text" name="password" id="password">
+                    <div class="form-group mt-4">
+                        <label for="password">Password:</label>
+                        <input required minlength="8" class="form-control" type="password" name="password" id="password">
                     </div>
-                    <input class="btn btn-primary w-100 mt-5" type="submit" value="Submit">
-                    <a href="/register" class="btn btn-success w-100 mt-2">Create Account</a>
+                    <button class="btn btn-primary w-100 mt-4" type="submit">Login</button>
                 </form>
+                <p class="mt-3"><a href="/register" class="btn btn-success w-100">Create an Account</a></p>
             </div>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
+
 </html>
