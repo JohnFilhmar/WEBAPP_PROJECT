83f3922d2a8415fe7ac68a5fcec274da061cf888
ampelme <olajohnfilhmar@gmail.com>
2023-11-22 01:47:52 +0800

    small change to policies

--- app/views/policies.php
+++ app/views/policies.php
@@ -96,7 +96,62 @@
     </div>
 
     <div class="container mx-auto p-8 text-center">
-        <h1 class="text-4xl font-bold mb-8">Privacy & Policies</h1>
+        <h1 class="text-4xl font-bold mb-8 border-t-4 mt-8 h-20 p-8">Terms of Services</h1>
+        <div class="grid grid-cols-1 md-grid-cols-2 lg:grid-cols-2 gap-8 border-t-4">
+            <section class="mb-8">.
+                <h1 class="mt-3 text-3xl font-extrabold mb-4">Website Terms and Conditions of Use</h1>
+
+                <h2 class="mt-3 text-2xl font-bold mb-4">1. Terms</h2>
+
+                <p>By accessing this Website, accessible from jms-eshop.com, you are agreeing to be bound by these Website Terms and Conditions of Use and agree that you are responsible for the agreement with any applicable local laws. If you disagree with any of these terms, you are prohibited from accessing this site. The materials contained in this Website are protected by copyright and trade mark law.</p>
+
+                <h2 class="mt-3 text-2xl font-bold mb-4">2. Use License</h2>
+
+                <p>Permission is granted to temporarily download one copy of the materials on Joemar MotorParts &amp; Services's Website for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:</p>
+
+                <ul>
+                    <li>modify or copy the materials;</li>
+                    <li>use the materials for any commercial purpose or for any public display;</li>
+                    <li>attempt to reverse engineer any software contained on Joemar MotorParts &amp; Services's Website;</li>
+                    <li>remove any copyright or other proprietary notations from the materials; or</li>
+                    <li>transferring the materials to another person or "mirror" the materials on any other server.</li>
+                </ul>
+
+                <p>This will let Joemar MotorParts &amp; Services to terminate upon violations of any of these restrictions. Upon termination, your viewing right will also be terminated and you should destroy any downloaded materials in your possession whether it is printed or electronic format. These Terms of Service has been created with the help of the <a href="https://www.termsofservicegenerator.net">Terms Of Service Generator</a>.</p>
+
+                <h2 class="mt-3 text-2xl font-bold mb-4">3. Disclaimer</h2>
+
+                <p>All the materials on Joemar MotorParts &amp; Services's Website are provided "as is". Joemar MotorParts &amp; Services makes no warranties, may it be expressed or implied, therefore negates all other warranties. Furthermore, Joemar MotorParts &amp; Services does not make any representations concerning the accuracy or reliability of the use of the materials on its Website or otherwise relating to such materials or any sites linked to this Website.</p>
+            </section>
+            <section class="mb-8">
+        
+                <h2 class="mt-3 text-2xl font-bold mb-4">4. Limitations</h2>
+
+                <p>Joemar MotorParts &amp; Services or its suppliers will not be hold accountable for any damages that will arise with the use or inability to use the materials on Joemar MotorParts &amp; Services's Website, even if Joemar MotorParts &amp; Services or an authorize representative of this Website has been notified, orally or written, of the possibility of such damage. Some jurisdiction does not allow limitations on implied warranties or limitations of liability for incidental damages, these limitations may not apply to you.</p>
+
+                <h2 class="mt-3 text-2xl font-bold mb-4">5. Revisions and Errata</h2>
+
+                <p>The materials appearing on Joemar MotorParts &amp; Services's Website may include technical, typographical, or photographic errors. Joemar MotorParts &amp; Services will not promise that any of the materials in this Website are accurate, complete, or current. Joemar MotorParts &amp; Services may change the materials contained on its Website at any time without notice. Joemar MotorParts &amp; Services does not make any commitment to update the materials.</p>
+
+                <h2 class="mt-3 text-2xl font-bold mb-4">6. Links</h2>
+
+                <p>Joemar MotorParts &amp; Services has not reviewed all of the sites linked to its Website and is not responsible for the contents of any such linked site. The presence of any link does not imply endorsement by Joemar MotorParts &amp; Services of the site. The use of any linked website is at the user's own risk.</p>
+
+                <h2 class="mt-3 text-2xl font-bold mb-4">7. Site Terms of Use Modifications</h2>
+
+                <p>Joemar MotorParts &amp; Services may revise these Terms of Use for its Website at any time without prior notice. By using this Website, you are agreeing to be bound by the current version of these Terms and Conditions of Use.</p>
+
+                <h2 class="mt-3 text-2xl font-bold mb-4">8. Your Privacy</h2>
+
+                <p>Please read our Privacy Policy.</p>
+
+                <h2 class="mt-3 text-2xl font-bold mb-4">9. Governing Law</h2>
+
+                <p>Any claim related to Joemar MotorParts &amp; Services's Website shall be governed by the laws of ph without regards to its conflict of law provisions.</p>
+
+            </section>
+        </div>
+        <h1 class="text-4xl font-bold mb-8 border-t-4 mt-8 h-20 p-8">Privacy & Policies</h1>
         <div class="grid grid-cols-1 md-grid-cols-2 lg:grid-cols-2 gap-8 border-t-4">
             <section class="mb-8">
                 <h2 class="mt-3 text-2xl font-bold mb-4">Privacy Policy</h2>
@@ -190,60 +245,6 @@
                 <h2 class="mt-3 text-2xl font-bold mb-4">Contact Us</h2>
 
                 <p>If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us.</p>
-            </section>
-        </div>
-        <div class="grid grid-cols-1 md-grid-cols-2 lg:grid-cols-2 gap-8 border-t-4">
-            <section class="mb-8">.
-                <h1 class="mt-3 text-3xl font-extrabold mb-4">Website Terms and Conditions of Use</h1>
-
-                <h2 class="mt-3 text-2xl font-bold mb-4">1. Terms</h2>
-
-                <p>By accessing this Website, accessible from jms-eshop.com, you are agreeing to be bound by these Website Terms and Conditions of Use and agree that you are responsible for the agreement with any applicable local laws. If you disagree with any of these terms, you are prohibited from accessing this site. The materials contained in this Website are protected by copyright and trade mark law.</p>
-
-                <h2 class="mt-3 text-2xl font-bold mb-4">2. Use License</h2>
-
-                <p>Permission is granted to temporarily download one copy of the materials on Joemar MotorParts &amp; Services's Website for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:</p>
-
-                <ul>
-                    <li>modify or copy the materials;</li>
-                    <li>use the materials for any commercial purpose or for any public display;</li>
-                    <li>attempt to reverse engineer any software contained on Joemar MotorParts &amp; Services's Website;</li>
-                    <li>remove any copyright or other proprietary notations from the materials; or</li>
-                    <li>transferring the materials to another person or "mirror" the materials on any other server.</li>
-                </ul>
-
-                <p>This will let Joemar MotorParts &amp; Services to terminate upon violations of any of these restrictions. Upon termination, your viewing right will also be terminated and you should destroy any downloaded materials in your possession whether it is printed or electronic format. These Terms of Service has been created with the help of the <a href="https://www.termsofservicegenerator.net">Terms Of Service Generator</a>.</p>
-
-                <h2 class="mt-3 text-2xl font-bold mb-4">3. Disclaimer</h2>
-
-                <p>All the materials on Joemar MotorParts &amp; Services's Website are provided "as is". Joemar MotorParts &amp; Services makes no warranties, may it be expressed or implied, therefore negates all other warranties. Furthermore, Joemar MotorParts &amp; Services does not make any representations concerning the accuracy or reliability of the use of the materials on its Website or otherwise relating to such materials or any sites linked to this Website.</p>
-            </section>
-            <section class="mb-8">
-        
-                <h2 class="mt-3 text-2xl font-bold mb-4">4. Limitations</h2>
-
-                <p>Joemar MotorParts &amp; Services or its suppliers will not be hold accountable for any damages that will arise with the use or inability to use the materials on Joemar MotorParts &amp; Services's Website, even if Joemar MotorParts &amp; Services or an authorize representative of this Website has been notified, orally or written, of the possibility of such damage. Some jurisdiction does not allow limitations on implied warranties or limitations of liability for incidental damages, these limitations may not apply to you.</p>
-
-                <h2 class="mt-3 text-2xl font-bold mb-4">5. Revisions and Errata</h2>
-
-                <p>The materials appearing on Joemar MotorParts &amp; Services's Website may include technical, typographical, or photographic errors. Joemar MotorParts &amp; Services will not promise that any of the materials in this Website are accurate, complete, or current. Joemar MotorParts &amp; Services may change the materials contained on its Website at any time without notice. Joemar MotorParts &amp; Services does not make any commitment to update the materials.</p>
-
-                <h2 class="mt-3 text-2xl font-bold mb-4">6. Links</h2>
-
-                <p>Joemar MotorParts &amp; Services has not reviewed all of the sites linked to its Website and is not responsible for the contents of any such linked site. The presence of any link does not imply endorsement by Joemar MotorParts &amp; Services of the site. The use of any linked website is at the user's own risk.</p>
-
-                <h2 class="mt-3 text-2xl font-bold mb-4">7. Site Terms of Use Modifications</h2>
-
-                <p>Joemar MotorParts &amp; Services may revise these Terms of Use for its Website at any time without prior notice. By using this Website, you are agreeing to be bound by the current version of these Terms and Conditions of Use.</p>
-
-                <h2 class="mt-3 text-2xl font-bold mb-4">8. Your Privacy</h2>
-
-                <p>Please read our Privacy Policy.</p>
-
-                <h2 class="mt-3 text-2xl font-bold mb-4">9. Governing Law</h2>
-
-                <p>Any claim related to Joemar MotorParts &amp; Services's Website shall be governed by the laws of ph without regards to its conflict of law provisions.</p>
-
             </section>
         </div>
     </div>
